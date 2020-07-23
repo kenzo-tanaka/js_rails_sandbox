4d9f7bf02ae834e9be981a12f07c44a795bc2165
kenzo <kenzou.kenzou104809@gmail.com>
2020-07-23 13:34:29 +0900

    render new task form when button clicked

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
 // jQuery
-// @see https://qiita.com/kazutosato/items/d47b7705ee545de4cb1a
 const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
   })
 )
 
